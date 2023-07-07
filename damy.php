29d5d1ac271eacb2f45c9d1bd69f1d000f03b265
Yumu12121 <122763148+Yumu12121@users.noreply.github.com>
2023-07-07 11:55:26 +0900

    Add files via upload

--- damy.php
+++ damy.php
@@ -10,17 +10,17 @@ $pdo = new PDO($dsn,$user,$password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 /* SQL文 */
-$sql = "INSERT INTO Room (room_name VALUES (?,? )";
+$sql = "INSERT INTO Room (room_name) VALUES (?)";
 $ps = $pdo->prepare($sql);
 
 /* データの受け取り */
-$_SESSION['$room_name'] = $_POST['room_name'];	//ルームの名前
+$_SESSION['room_name'] = $_POST['room_name'];	//ルームの名前
 
 /* ??の部分に入力する値 */
-$ps->bindValue(1,$_SESSION['$room_name'],PDO::PARAM_STR);
+$ps->bindValue(1,$_SESSION['room_name'],PDO::PARAM_STR);
 
 /* 実行 */
-$ps->execute()
+$ps->execute();
 header("Location:http://cold-yoron-2957.itigo.jp/ChatRoom.php");		/* チャットルーム画面に遷移　*/ 
 exit();
 ?>
